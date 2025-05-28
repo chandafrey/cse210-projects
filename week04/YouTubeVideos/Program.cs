2fe8645d1ca91f05f1d73d72813bbc27a7a91785
chandafrey <chanda.frey09@gmail.com>
2025-05-28 12:49:02 -0600

    finished my YouTube Videos program

--- week04/YouTubeVideos/Program.cs
+++ week04/YouTubeVideos/Program.cs
@@ -5,5 +5,67 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the YouTubeVideos Project.");
+
+// Once you have the classes in place, write a program that:
+// - creates 3-4 videos, 
+// - sets the appropriate values, 
+// - and for each one add a list of 3-4 comments (with the commenter's name and text). 
+// Put each of these videos in a list.
+    
+    // Create comments with commenter's name
+    // These must be declared and assigned BEFORE THEY ARE USED!
+    Comment comment1 = new Comment("Alice", "Great video!");
+    Comment comment2 = new Comment("Bob", "Thanks for sharing!");   
+    Comment comment3 = new Comment("Charlie", "Very informative!");
+    Comment comment4 = new Comment("Dave", "I learned a lot from this!");
+    Comment comment5 = new Comment("Eve", "This was helpful, thanks!");
+    Comment comment6 = new Comment("Frank", "I appreciate the effort you put into this video!");
+
+
+    // Create some video objects with comments
+    // Each video will have a title, author, length in seconds, and a list of comments ADDED
+    Video video1 = new Video("Understanding C# Classes", "Jane Doe", 300);
+    video1.AddComment(comment1);
+    video1.AddComment(comment2);
+    video1.AddComment(comment3);
+    
+    Video video2 = new Video("Advanced C# Programming", "John Smith", 600);
+    video2.AddComment(comment4);
+    video2.AddComment(comment5);
+    video2.AddComment(comment6);
+
+    Video video3 = new Video("C# for Beginners", "Alice Johnson", 450);
+    video3.AddComment(comment1);
+    video3.AddComment(comment3);
+    video3.AddComment(comment5);
+
+// -------------------------------------------------------------
+  
+//  Then, have your program iterate through the list of videos and for each one, 
+// - display the title, author, length, 
+// - number of comments (from the method) 
+// - and then list out all of the comments for that video. 
+// - Repeat this display for each video in the list.
+   
+    // Create a LIST OF VIDEOS and loop through them to display their data
+    // we can add the videos {video1, ...like this when we know all the items 
+    // we want to add at the time of creating the list}
+    List<Video> videos = new List<Video> {video1, video2, video3};
+    foreach (Video video in videos)
+    {
+        Console.WriteLine(video.GetVideoData());
+        Console.WriteLine($"Number of Comments: {video.GetNumberOfComments()}");
+        // ALSO display a list of comments for each video
+        foreach (string comment in video.GetDisplayComments())
+        {
+            Console.WriteLine(comment);          
+        }
+
+        // Add visual separation between videos
+        Console.WriteLine(new string('-', 40));
+    }
+
+
+
     }
 }
---
+++ week04/YouTubeVideos/Video.cs
@@ -0,0 +1,82 @@
+using System;
+using System.Collections.Generic;
+
+public class Video
+{
+    // ATTRIBUTE/VARIABLE DECLARATION
+    // We need to track title, author, length(in seconds), and 
+    // a list of comments for each video.
+    // --------------------------------
+    // include "using System.Collections.Generic;" at the top of the file if using List<>
+    // --------------------------------
+    // -Visibility of variable (private, public, protected, etc.)
+    // -Type of variable (string, int, etc.)
+    // -Name of variable
+    private string _title;
+    private string _author;
+    private int _length; // in seconds
+    private List<Comment> _comments;
+
+
+    // CONSTRUCTOR
+    public Video(string title, string author, int length)
+    {
+        // We are setting the _title, _author, and _length variables to the values
+        // passed in as parameters. This is a good choice because it is easy to remember
+        // and make the connection between the two.
+        // The underscore is used to indicate that these are private member variables.
+        _title = title;
+        _author = author;
+        _length = length;
+        _comments = new List<Comment>(); // Initialize an empty list if no comments are provided
+    }
+
+    // METHODS
+
+    // 1. GetVideoData()
+    // This method returns a string that contains the video title, author, and length
+    public string GetVideoData()
+    {
+        return $"Video Title: {_title}, Author: {_author}, Length: {_length} seconds";
+    }
+
+    // 2. AddComment()
+    // Each video has responsibility to store a list of comments, 
+    // This method will take in a new comment and add it to the list of comments
+    // Inside the parentheses (type of parameter) - Use Comment class as data type
+    // Inside the parentheses (name of parameter) - Use newComment
+    // This method will not return anything, so return type is void
+    public void AddComment(Comment newComment)
+    {
+        // use .Add to the _comments list that was created above in the Constructor
+        _comments.Add(newComment);
+    }
+
+    // 3.  GetDisplayComments()
+    // This method will loop through each comment in the _comments List and RETURN the list of comments
+    public List<string> GetDisplayComments()
+    {
+        List<string> commentList = new List<string>();
+        foreach (Comment comment in _comments)
+        {
+            commentList.Add(comment.GetCommentData());
+        }
+        // *** Notice the return is outside the foreach loop:
+        // - We want to add ALL the comments to the list
+        // - If we returned inside the loop, it would stop after the first comment
+        return commentList;
+    }
+
+    // 4.  GetNumberOfComments()
+    // Each video should have a method to return the number of comments
+    // This method will Count and RETURN the number of comments in the list
+    public int GetNumberOfComments()
+    {
+        // ✅ Rule of Thumb:
+        // For counting items in a list → use .Count
+        // For doing something with each item → use foreach
+        return _comments.Count;
+    }
+    
+
+}
